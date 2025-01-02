9c7c27c2df5b2d605a0c7df7d63469b8c691108f
EGGS-Artesvisuales <120066289+EGGS-Artesvisuales@users.noreply.github.com>
2025-01-02 05:45:17 -0300

    Create Mermaid.js

---
+++ _includes/Mermaid.js
@@ -0,0 +1,42 @@
+```mermaid
+graph TD
+    A[<a href='/mundo-exterior.html'>Representación del Mundo</a>]
+    A1[<a href='/tangible.html'>Lo Tangible</a>]
+    A2[<a href='/intangible.html'>Lo Intangible</a>]
+    A1a[<a href='/saturacion.html'>La Saturación</a>]
+    A1a1[<a href='/peste.html'>La Peste</a>]
+    A1a2[<a href='/invisible.html'>Lo Invisible</a>]
+    A1b[<a href='/tecnologia.html'>La Tecnología</a>]
+    A1b1[<a href='/ia.html'>La IA</a>]
+    A1b2[<a href='/espacial.html'>Lo Espacial</a>]
+    A1b3[<a href='/telecomunicaciones.html'>Telecomunicaciones</a>]
+    A2a[<a href='/capitalismo.html'>El Capitalismo</a>]
+    A2b[<a href='/popular.html'>Lo Popular</a>]
+    A2c[<a href='/ancestral.html'>Lo Ancestral</a>]
+    B[<a href='/accion.html'>Acción en el Mundo</a>]
+    B1[<a href='/subrepticio.html'>Lo Subrepticio</a>]
+    B2[<a href='/comunitario.html'>Lo Comunitario</a>]
+    B3[<a href='/docencia.html'>Docencia y Talleres</a>]
+    B4[<a href='/servicios.html'>Servicios</a>]
+    C[<a href='/interior.html'>Exploración del Mundo Interior</a>]
+    C1[<a href='/psicologia.html'>Arte y Psicología</a>]
+    C2[<a href='/espiritualidad.html'>Arte y Espiritualidad</a>]
+
+    A --> A1
+    A --> A2
+    A1 --> A1a
+    A1 --> A1b
+    A1a --> A1a1
+    A1a --> A1a2
+    A1b --> A1b1
+    A1b --> A1b2
+    A1b --> A1b3
+    A2 --> A2a
+    A2 --> A2b
+    A2 --> A2c
+    B --> B1
+    B --> B2
+    B --> B3
+    B --> B4
+    C --> C1
+    C --> C2
