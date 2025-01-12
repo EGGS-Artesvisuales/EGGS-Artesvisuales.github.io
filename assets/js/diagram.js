05868352d8f35dbbe316f4574e1cf9b9395b1547
EGGS-Artesvisuales <120066289+EGGS-Artesvisuales@users.noreply.github.com>
2025-01-12 05:26:36 -0300

    Update diagram.js

--- assets/js/diagram.js
+++ assets/js/diagram.js
@@ -1,6 +1,8 @@
-// Agregar interactividad al diagrama
-document.querySelectorAll('.node a').forEach((link) => {
+document.addEventListener('DOMContentLoaded', function() {
+  // Agregar interactividad al diagrama una sola vez
+  document.querySelectorAll('.node a').forEach(link => {
     link.addEventListener('click', () => {
-        console.log(`Navegando a: ${link.title}`);
+      console.log(`Navegando a: ${link.title}`);
     });
+  });
 });
