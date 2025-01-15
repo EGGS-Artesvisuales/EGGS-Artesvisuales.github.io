e2b625d1e1d8475db551f0d8a97f42d1e50ea871
EGGS-Artesvisuales <120066289+EGGS-Artesvisuales@users.noreply.github.com>
2025-01-15 04:37:58 -0300

    Update menu.js

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -5,8 +5,12 @@ document.addEventListener('DOMContentLoaded', () => {
   if (navToggle && navMenu) {
     // Al hacer clic en el ícono hamburguesa
     navToggle.addEventListener('click', () => {
+      const isExpanded = navToggle.getAttribute('aria-expanded') === 'true' || false;
+      navToggle.setAttribute('aria-expanded', !isExpanded);
+      
       navMenu.classList.toggle('active'); // Alternar visibilidad del menú
       navToggle.classList.toggle('active'); // Cambiar estado del botón
+      document.body.classList.toggle('no-scroll'); // Evitar scroll
     });
 
     // Cerrar el menú al hacer clic en cualquier enlace del menú
@@ -15,10 +19,11 @@ document.addEventListener('DOMContentLoaded', () => {
       link.addEventListener('click', () => {
         navMenu.classList.remove('active');
         navToggle.classList.remove('active');
+        navToggle.setAttribute('aria-expanded', false);
+        document.body.classList.remove('no-scroll');
       });
     });
   } else {
     console.warn('Elementos del menú no encontrados.');
   }
 });
-
