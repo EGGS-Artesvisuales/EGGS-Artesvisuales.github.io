2ca6c8c897b4765ecb98a0a49bc1a8db4cb9ad76
EGGS-Artesvisuales <120066289+EGGS-Artesvisuales@users.noreply.github.com>
2025-02-09 18:42:50 -0300

    Update carousel-init.js

--- assets/js/carousel-init.js
+++ assets/js/carousel-init.js
@@ -2,7 +2,8 @@ $(function() {
   var navText = ["&#9664;", "&#9654;"];
 
   // Inicialización para el carrusel de imágenes (versión normal)
-  $('.carousel-imagen').owlCarousel({
+  // Se excluyen los contenedores que sean del tipo large.
+  $('.carousel-imagen:not(.owl-carousel-large)').owlCarousel({
     items: 3,
     loop: true,
     margin: 10,
@@ -30,14 +31,15 @@ $(function() {
   });
 
   // Inicialización para el carrusel de imágenes grandes (slide grande)
-  // Se elimina "items" ya que autoWidth: true se encarga de respetar el ancho definido en CSS.
-  $('.carousel-grandes').owlCarousel({
+  // Se elimina "items" ya que autoWidth: true respeta el ancho definido en CSS.
+  $('.owl-carousel-large.carousel-imagen').owlCarousel({
     loop: true,
-    margin: 0,           // Se elimina el margin en JS para usar el gap definido en CSS
+    margin: 0,           // Margin eliminado en JS para usar el gap definido en CSS.
     nav: true,
     dots: false,
     navText: navText,
-    autoWidth: true,     // Se respeta el ancho definido en CSS (ej. 800px)
+    autoWidth: true,     // Respeta el ancho definido en CSS (ej. 800px)
     center: true
   });
 });
+
