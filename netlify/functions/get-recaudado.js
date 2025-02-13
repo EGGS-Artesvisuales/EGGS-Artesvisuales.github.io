95d42df366fefad9f6aeed306c2068f96303a614
EGGS-Artesvisuales <120066289+EGGS-Artesvisuales@users.noreply.github.com>
2025-02-13 09:21:18 -0300

    Update get-recaudado.js

--- netlify/functions/get-recaudado.js
+++ netlify/functions/get-recaudado.js
@@ -1,7 +1,8 @@
-exports.handler = async function () {
-    return {
-        statusCode: 200,
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ recaudado: 1000 }) // Simulación de datos
-    };
+
+exports.handler = async function(event, context) {
+  return {
+    statusCode: 200,
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ recaudado: 0 })
+  };
 };
