84bcef405cb79bd4cd3fdc7470f1de7b50e0e725
EGGS-Artesvisuales <120066289+EGGS-Artesvisuales@users.noreply.github.com>
2025-04-04 00:20:42 -0300

    Update carousel-init.js

--- assets/js/carousel-init.js
+++ assets/js/carousel-init.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+  // Carrusel de imágenes
   $('.carousel-imagen').owlCarousel({
     items: 1,
     loop: true,
@@ -6,10 +7,8 @@ $(document).ready(function() {
     nav: true,
     dots: true,
     dotsEach: 1,
-    // Elimina "center: true" para que la imagen se muestre completa
     startPosition: 0,
     stagePadding: 30,
-
     navText: ["&#9664;", "&#9654;"],
     responsive: {
       0: {
@@ -18,33 +17,41 @@ $(document).ready(function() {
       768: {
         stagePadding: 30
       }
+    },
+    onInitialized: function() {
+      $('.carousel-imagen').trigger('refresh.owl.carousel');
     }
   });
-});
-
 
   // Carrusel fancy
-$('.carousel-fancy').owlCarousel({
-  loop: true,
-  margin: 5,
-  nav: true,
-  dots: true,
-  navText: [
-    '<span class="owl-nav-prev">&#10094;</span>',
-    '<span class="owl-nav-next">&#10095;</span>'
-  ],
-  responsiveClass: true,
-  responsive: {
-    0: {
-      items: 2,   // Para dispositivos muy pequeños
-      dots: true
-    },
-    600: {
-      items: 3,   // Para dispositivos medianos
-      nav: false
-    },
-    1000: {
-      items: 4   // Para pantallas grandes
+  $('.carousel-fancy').owlCarousel({
+    loop: true,
+    margin: 5,
+    nav: true,
+    dots: true,
+    navText: [
+      '<span class="owl-nav-prev">&#10094;</span>',
+      '<span class="owl-nav-next">&#10095;</span>'
+    ],
+    responsiveClass: true,
+    responsive: {
+      0: {
+        items: 2,   // Para dispositivos muy pequeños
+        dots: true
+      },
+      600: {
+        items: 3,   // Para dispositivos medianos
+        nav: false
+      },
+      1000: {
+        items: 4   // Para pantallas grandes
+      }
     }
-  }
+  });
+
+  // Forzar refresh al terminar de cargar la página
+  $(window).on('load', function() {
+    $('.carousel-imagen').trigger('refresh.owl.carousel');
+    $('.carousel-fancy').trigger('refresh.owl.carousel');
+  });
 });
