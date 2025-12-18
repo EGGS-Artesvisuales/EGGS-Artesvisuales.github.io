3ccdc9aee63651d37b5ce51038b7f6b27b2a8c64
EGGS-Artesvisuales <120066289+EGGS-Artesvisuales@users.noreply.github.com>
2025-12-18 05:33:24 -0300

    Update mermaid-init.js

--- assets/js/mermaid-init.js
+++ assets/js/mermaid-init.js
@@ -4,13 +4,6 @@ document.addEventListener("DOMContentLoaded", () => {
   mermaid.initialize({
     startOnLoad: true,
     theme: "base",
-    themeVariables: {
-      primaryColor: "#f5f5f5",
-      primaryTextColor: "#111",
-      primaryBorderColor: "#111",
-      lineColor: "#111",
-      fontFamily: "inherit",
-      fontSize: "16px"
-    }
+    flowchart: { htmlLabels: false } // evita dobles textos por foreignObject
   });
 });
